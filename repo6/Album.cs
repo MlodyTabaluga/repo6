50d7e89900a278b701e2a18aa61e28374b2d1a6f
Milosz Szubryt <mszubryt2005@gmail.com>
2024-01-23 20:26:01 +0100

    dodano klase Album

---
+++ repo6/Album.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repo6
+{
+    public class Album
+    {
+        public string Name { get; set; }
+        public ObservableCollection<Photo> Photos { get; set; }
+    }
+}
